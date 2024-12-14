a8b4a546be32e629f7e55c2312d9c3debeed6426
thebeard7 <beardkn@miamioh.edu>
2024-12-13 21:05:13 -0500

    Delete pll.v

--- pll.v
+++
@@ -1,7 +0,0 @@
-module pll (
-    input inclk0,
-    output c0
-);
-    // This is a placeholder. In Quartus, you'll use the IP Catalog 
-    // to generate a PLL that converts 50 MHz to 25.175 MHz
-endmodule
